08d3e4c9ed13ade609c043c29d440c51b180ac96
solomonstaby <solu27000@gmail.com>
2016-10-03 17:38:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+//submit name
+var nameInput =document.getElementById('name');
+var name = varInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+    //make a request to the server and send the name
+    
+    //capture a liust of names and render it as a list
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0,i<names.length;i++){
+        list += '<li>' + names[i] + '<li>';
+    }
+    var ul =document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
